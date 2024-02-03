f95466e8ae8de817803099df99960bd11f92c98a
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-02-03 09:50:01 -0800

    Add files via upload

---
+++ set2.py
@@ -0,0 +1,35 @@
+# animals ={"dog","cat","monkey","horse"}
+# print(animals)
+# animals.add("cow")
+# print(animals)
+
+# animals ={"dog","cat","monkey","horse","bear"}
+# wild_animals ={"tiger","snake","rat","bear"}
+# animals.update(wild_animals,{"dolphin"})
+# print(animals)
+
+# animals ={"dog","cat","monkey","horse","bear"}
+# animals.discard("dolphin")
+# print(animals)
+
+# animals ={"dog","cat","monkey","horse","bear"}
+# animals.remove("dolphin")
+# print(animals)
+
+# animals ={"dog","cat","monkey","horse","bear"}
+# animals.clear()
+# print(animals)
+
+# animals ={"dog","cat","monkey","horse","bear"}
+# print("tiger" in animals)
+
+
+# animals ={"dog","cat","monkey","horse","bear"}
+# wild_animals ={"tiger","snake","rat","bear"}
+# all_animals = animals |wild_animals
+# print(all_animals)
+
+animals ={"dog","cat","monkey","horse","bear"}
+wild_animals ={"tiger","snake","rat","bear"}
+all_animals = animals & wild_animals
+print(all_animals)
